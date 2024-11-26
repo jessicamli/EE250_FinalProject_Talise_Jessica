e63fb62756117a3628c02dd6887097036a1b314c
Talise Baker-Matsuoka <tbakermatsuoka@gmail.com>
2024-11-25 17:26:45 -0800

    testing

--- local_sub.py
+++ local_sub.py
@@ -58,13 +58,11 @@ if __name__ == '__main__':
 
         # Load the model and figure out the predicted square
         model = load_model('distmodel.h5')
-        prediction = model.predict([sensor_dist1, sensor_dist2])
-        print(prediction)
-        prediction = np.round(prediction)
-        print(prediction)
+        prediction = model.predict(np.array([[sensor_dist1, sensor_dist2]]))
         prediction = prediction.flatten()
 
-        square = prediction[0]
+        square = np.argmax(prediction)
+
         print(prediction)
         print(square)
 
